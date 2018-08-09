61b517795dbc030a33dcd0dc15969491c573604b
e <chengzhelin@qaqgame.com>
2018-08-09 17:20:02 +0800

    保存

--- routes/ApiProxy.js
+++ routes/ApiProxy.js
@@ -5,10 +5,13 @@
 const Router = require('koa-router');
 const router = new Router();
 const rp = require('request-promise-native');
+const fs = require('fs');
+let key = fs.readFileSync('./key.txt').toString().split(/\r\n|\n/);
+console.log(key[1]);
 
 let {version} = require('../config');
 router.prefix(`/${version}/proxy`);
-let key = fs.readFileSync('key.txt').toString();
+
 
 // 获取指定成员基本信息
 router.post('/test', async (ctx, next) => {
@@ -18,11 +21,37 @@ router.post('/test', async (ctx, next) => {
     await next();
 });
 
-router.post('/ip/:key', async (ctx, next) => {
-    if (ctx.params.key === key && !ctx.body) {
-        ctx.body = await rp({
-             url: 'http://gec.ip3366.net/api/?key=20180808185702582&getnum=1&anonymoustype=3&filter=1&area=1&order=2&formats=2&proxytype=1'
-        });
+let saveData = {
+    time: 0,
+    data: {}
+};
+
+router.get('/ip/:key', async (ctx, next) => {
+    let time = new Date().getTime();
+    if (ctx.params.key === key[1] && !ctx.body) {
+        if (time - saveData.time > 3 * 60 * 1000) {
+            let data = await rp({
+                url: 'http://api.ip.data5u.com/socks/get.html?order=c8d7e0ae61e6e935125e3bf0d8a2b5dd&ttl=1&json=1&random=true&type=1&sep=3'
+            });
+            data = JSON.parse(data);
+
+            if (!data.success) {
+                saveData.data = {
+                    url: '',
+                    outTime: time
+                };
+            } else {
+                data = data.data[0];
+                saveData.data = {
+                    url: `http://${data.ip}:${data.port}`,
+                    outTime: new Date(data.outTime).getTime()
+                };
+            }
+
+            saveData.time = time;
+        }
+
+        ctx.body = saveData.data;
     }
     await next();
 });
--- routes/ApiStar.js
+++ routes/ApiStar.js
@@ -12,12 +12,14 @@ let {version} = require('../config');
 router.prefix(`/${version}/newstar`);
 
 const {queryNewStarData} = require('../database/mysql.js');
-let key = fs.readFileSync('key.txt').toString();
+const fs = require('fs');
+let key = fs.readFileSync('./key.txt').toString().split(/\r\n|\n/);
+console.log(key[0]);
 
 // 获取指定成员信息
 router.get('/:type/:key', async (ctx, next) => {
   let type = ctx.params.type;
-  if (ctx.params.key === key && /^(all|new)$/.test(type)) {
+  if (ctx.params.key === key[0] && /^(all|new)$/.test(type)) {
     let data = await queryNewStarData(type);
     let items = [['日期', '微博昵称', '排名', '总评分', '阅读数', '阅读评分', '互动值', '互动评分' , '社会影响力', '影响力评分', '爱慕值', '爱慕值评分']];
     let len = data.length;
